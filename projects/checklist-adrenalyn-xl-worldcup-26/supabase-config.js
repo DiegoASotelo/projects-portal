24e2b979f7ef9f0abda29b9b907b0bc2c751cb9e
root <root@vmi3244238.contaboserver.net>
2026-05-12 18:11:13 +0200

    Confirm Supabase auth setup state

--- projects/checklist-adrenalyn-xl-worldcup-26/supabase-config.js
+++ projects/checklist-adrenalyn-xl-worldcup-26/supabase-config.js
@@ -1,5 +1,5 @@
 window.CHECKLIST_SUPABASE_CONFIG = {
   url: 'https://tbowfxguhgtldvwmpisz.supabase.co',
   anonKey: 'sb_publishable_OZ9d5IkpJu-zd3GI38vtgw_cyuir6Uf',
-  googleClientId: 'GOOGLE_CLIENT_ID_PENDING'
+  googleClientId: 'USE_SUPABASE_GOOGLE_OAUTH'
 };
